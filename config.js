37baabfbc14b8a2ceb204cc991e1b391faf29cc2
shafiullah150 <unknownkhan150@gmail.com>
2025-09-21 01:25:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW Sʜᴀʙᴀɴ-Mᴅ 𓅓*",
 // set the auto reply massage on status reply
-WELCOME: process.env.WELCOME || "false",
+WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group  
@@ -27,11 +27,11 @@ BOT_NAME: process.env.BOT_NAME || "SHABAN-MD V5",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "😭,💗,💀🥲,🙂,👀,🔥,😂,😁,🙈,🐺,😙,⚠️🔗,♐,👾,😃,🤣,😉,😗,😘,🙃,🥺,🤭,🙄,😒,🤨,😓,😫,🥵,🤧,🤓,☠️,👻,🌚,🤡,😹,💯,🕳️,⚡,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍,💝,💖,💓,💞,💕,♥️,💋,💔,🫂,🗣️,👣،🦠,👅,🙌🏻,💅🏻,🤸🏻,🧟,💐,🌹,🥀, 🌷,🌸,💮,🍂,🍄,🍁,🌾,🌱,🍃,☘️,🪴,⛄,🌋,🌈,🌬️,🌀,☔,🌝,✨,🌎,🐵,🐻,🐼,🐰,🦄,🦎,🐉,🐊,🐸,🐇,🐎,🐑,🦥,🐒,🦧,🦫,🦦,🦇,🐦,🐓,🐔,🐣, 🦅,🦜,🕊️,🦚,🐧,🦭,🦈,🐬,🦑,🐜,🦀,🪲,🐝,🐞,🦋,🐛,🐾,🍒,🍉,🍊,🍌,🍨,🍦,🎂,🍭,🍬,🧃,🍻, 🥢,🔪,🚕,🚔,🚀,🗿,🗽,🎠,🎡,🏩,🎉,🎈,🎀,🎁,🪔,🪅,🎐,🎏,🎑,🎋,🎃,🎄,🎗️,🪁,🪀,🎳,♟️,👾,🎧,🎥💸,🧸,🧻,🚿,🪒,💍,💉,🩹,🪚,✂️,✒️,📍,🗑️,🕐,📿,🔮,🔐,♠️,❌,❓,🚭,⛔,⚜️,🎶,🚩,🏁,🎌,🇮🇳,💭,🪣,📡,👡,👑,🪠,🥊,🎎,💔,🛩️, 🍹,",
 // chose custom react emojis by yourself  
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "249 10 216 9717",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
 // add bot owner name
@@ -41,7 +41,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-
 // add img for alive msg 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -49,7 +49,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups  
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
@@ -59,12 +59,12 @@ READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
 DEV: process.env.DEV || "923059395959",
 //replace with your whatsapp number        
-ANTI_CALL: process.env.ANTI_CALL || "false",
+ANTI_CALL: process.env.ANTI_CALL || "true",
 REJECT_MSG: process.env.REJECT_MSG || "*_SOORY MY BOSS IS BUSY PLEASE DONT CALL ME_*",
-ANTI_DELETE: process.env.ANTI_DELETE || "false",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
